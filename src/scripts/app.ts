492764640526cda4b2606b6b7c9c8287ce38a27f
Davina Leong <leong.shi.yun@gmail.com>
2023-09-15 11:04:14 +0800

    Added commonly used functions

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,6 +1,8 @@
 console.log(`app.ts loaded.`)
 
 /// Variables
+const DATA_ELEMENT_ATTR = `data-element`
+const DATA_ACTIVE_ATTR = `data-active`
 
 /// Functions
 function main() {
@@ -11,6 +13,43 @@ function main() {
 
 function logFunction(name: string, params: any = {}): void {
   console.log(`fn ${name}(${JSON.stringify(params)})`)
+}
+
+function getElement(name: string, parent?: HTMLElement): HTMLElement | null {
+  logFunction(`getElement`, { name, parent })
+
+  if (parent) {
+    return parent.querySelector(`[${DATA_ELEMENT_ATTR}="${name}"]`)
+  }
+
+  return document.querySelector(`[${DATA_ELEMENT_ATTR}="${name}"]`)
+}
+
+function getAllElements(
+  name: string,
+  parent?: HTMLElement
+): NodeListOf<Element> {
+  logFunction(`getElement`, { name, parent })
+
+  if (parent) {
+    return parent.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${name}"]`)
+  }
+
+  return document.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${name}"]`)
+}
+
+function toggleAttribute(
+  element: HTMLElement,
+  toggle: boolean = false,
+  attr: string = DATA_ACTIVE_ATTR
+): void {
+  logFunction(`toggleAttribute`, { element, toggle, attr })
+
+  if (toggle) {
+    element.setAttribute(attr, String(toggle))
+  } else {
+    element.removeAttribute(attr)
+  }
 }
 
 /// Execute
