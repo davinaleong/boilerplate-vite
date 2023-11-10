388970c11775aa5f64071bf59ac8ff61a9728fac
Davina Leong <davina.leong.sy@gmail.com>
2023-11-10 17:44:58 +0800

    Fixed build issues

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,41 +1,44 @@
-console.log(`app.ts loaded.`)
+console.log(`app.ts loaded.`);
+
+// Load CSS
+import "../styles/app.scss";
 
 /// Variables
-const DATA_ELEMENT_ATTR = `data-element`
-const DATA_ACTIVE_ATTR = `data-active`
+const DATA_ELEMENT_ATTR = `data-element`;
+const DATA_ACTIVE_ATTR = `data-active`;
 
 /// Functions
 function main() {
-  logFunction(`main`)
+  logFunction(`main`);
 
   // Logic goes here
 }
 
 function logFunction(name: string, params: any = {}): void {
-  console.log(`fn ${name}(${JSON.stringify(params)})`)
+  console.log(`fn ${name}(${JSON.stringify(params)})`);
 }
 
 function getElement(name: string, parent?: HTMLElement): HTMLElement | null {
-  logFunction(`getElement`, { name, parent })
+  logFunction(`getElement`, { name, parent });
 
   if (parent) {
-    return parent.querySelector(`[${DATA_ELEMENT_ATTR}="${name}"]`)
+    return parent.querySelector(`[${DATA_ELEMENT_ATTR}="${name}"]`);
   }
 
-  return document.querySelector(`[${DATA_ELEMENT_ATTR}="${name}"]`)
+  return document.querySelector(`[${DATA_ELEMENT_ATTR}="${name}"]`);
 }
 
 function getAllElements(
   name: string,
   parent?: HTMLElement
 ): NodeListOf<Element> {
-  logFunction(`getElement`, { name, parent })
+  logFunction(`getElement`, { name, parent });
 
   if (parent) {
-    return parent.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${name}"]`)
+    return parent.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${name}"]`);
   }
 
-  return document.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${name}"]`)
+  return document.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${name}"]`);
 }
 
 function toggleAttribute(
@@ -43,14 +46,14 @@ function toggleAttribute(
   toggle: boolean = false,
   attr: string = DATA_ACTIVE_ATTR
 ): void {
-  logFunction(`toggleAttribute`, { element, toggle, attr })
+  logFunction(`toggleAttribute`, { element, toggle, attr });
 
   if (toggle) {
-    element.setAttribute(attr, String(toggle))
+    element.setAttribute(attr, String(toggle));
   } else {
-    element.removeAttribute(attr)
+    element.removeAttribute(attr);
   }
 }
 
 /// Execute
-document.addEventListener(`DOMContentLoaded`, main)
+document.addEventListener(`DOMContentLoaded`, main);
