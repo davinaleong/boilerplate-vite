dab7f63f087189ac8dc704cea098dce402624e0e
Davina Leong <davina.leong.sy@gmail.com>
2023-11-17 18:34:29 +0800

    Updated scripts

--- src/scripts/app.ts
+++ src/scripts/app.ts
@@ -1,57 +1,31 @@
-console.log(`app.ts loaded.`);
-
-// Load CSS
+console.log(`app.js loaded`);
 import "../styles/app.scss";
+import { now, logFunction, getElement } from "./helpers";
+import { Config } from "./config";
 
 /// Variables
-const DATA_ELEMENT_ATTR = `data-element`;
-const DATA_ACTIVE_ATTR = `data-active`;
 
 /// Functions
 function main() {
   logFunction(`main`);
 
-  // Logic goes here
+  renderCopyright();
 }
 
-function logFunction(name: string, params: any = {}): void {
-  console.log(`fn ${name}(${JSON.stringify(params)})`);
-}
+function renderCopyright() {
+  logFunction(`renderCopyright`);
 
-function getElement(name: string, parent?: HTMLElement): HTMLElement | null {
-  logFunction(`getElement`, { name, parent });
+  // Format: The Beloved's Blog &copy; Davina Leong, 2022-2024
+  const copyrightEl: HTMLElement | null = getElement(`copyright`);
+  if (copyrightEl) {
+    const yearHtml =
+      Config.year === now.getFullYear()
+        ? `${Config.year}`
+        : `${Config.year} &ndash; ${now.getFullYear()}`;
 
-  if (parent) {
-    return parent.querySelector(`[${DATA_ELEMENT_ATTR}="${name}"]`);
-  }
-
-  return document.querySelector(`[${DATA_ELEMENT_ATTR}="${name}"]`);
-}
-
-function getAllElements(
-  name: string,
-  parent?: HTMLElement
-): NodeListOf<Element> {
-  logFunction(`getElement`, { name, parent });
-
-  if (parent) {
-    return parent.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${name}"]`);
-  }
-
-  return document.querySelectorAll(`[${DATA_ELEMENT_ATTR}="${name}"]`);
-}
-
-function toggleAttribute(
-  element: HTMLElement,
-  toggle: boolean = false,
-  attr: string = DATA_ACTIVE_ATTR
-): void {
-  logFunction(`toggleAttribute`, { element, toggle, attr });
-
-  if (toggle) {
-    element.setAttribute(attr, String(toggle));
-  } else {
-    element.removeAttribute(attr);
+    copyrightEl.innerHTML = `
+        ${Config.appName} &copy; ${Config.author}, ${yearHtml}
+        `;
   }
 }
 
